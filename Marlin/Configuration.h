dd10ce9e14c5998c4a65f2b898721f8f2372f377
Michael <mluckau@gmail.com>
2020-12-28 20:05:29 +0100

    PID Werte angepasst

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -494,9 +494,9 @@
     #define DEFAULT_Ki_LIST {   1.08,   1.08 }
     #define DEFAULT_Kd_LIST { 114.00, 114.00 }
   #else
-    #define DEFAULT_Kp  22.20
-    #define DEFAULT_Ki   1.08
-    #define DEFAULT_Kd 114.00
+    #define DEFAULT_Kp  12.54
+    #define DEFAULT_Ki   0.72
+    #define DEFAULT_Kd  54.28
   #endif
 #endif // PIDTEMP
 
