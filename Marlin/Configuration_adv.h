1a4b127637af9314581c7dd0aecfc74c922ee86c
Michael <mluckau@gmail.com>
2021-04-02 10:04:15 +0200

    Disable Lin_advanced

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1801,7 +1801,7 @@
  *
  * See https://marlinfw.org/docs/features/lin_advance.html for full instructions.
  */
-#define LIN_ADVANCE
+//#define LIN_ADVANCE
 #if ENABLED(LIN_ADVANCE)
   //#define EXTRA_LIN_ADVANCE_K // Enable for second linear advance constants
   #define LIN_ADVANCE_K 0.00   // Unit: mm compression per 1mm/s extruder speed
@@ -2646,7 +2646,7 @@
    */
   #define STEALTHCHOP_XY
   #define STEALTHCHOP_Z
-  //#define STEALTHCHOP_E
+  #define STEALTHCHOP_E
 
   /**
    * Optimize spreadCycle chopper parameters by using predefined parameter sets
