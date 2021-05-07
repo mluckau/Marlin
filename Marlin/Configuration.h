0e9086081d2d5001ef3cfade619a4f4e95fb6060
Michael <mluckau@gmail.com>
2021-05-07 21:28:03 +0200

    Headbed PID

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -561,9 +561,14 @@
 
   // 120V 250W silicone heater into 4mm borosilicate (MendelMax 1.5+)
   // from FOPDT model - kp=.39 Tp=405 Tdead=66, Tc set to 79.2, aggressive factor of .15 (vs .1, 1, 10)
-  #define DEFAULT_bedKp 10.00
-  #define DEFAULT_bedKi .023
-  #define DEFAULT_bedKd 305.4
+  //#define DEFAULT_bedKp 10.00
+  //#define DEFAULT_bedKi .023
+  //#define DEFAULT_bedKd 305.4
+
+  //Tronxy X5SA-Pro
+  #define DEFAULT_bedKp 258.3625
+  #define DEFAULT_bedKi 51.6209
+  #define DEFAULT_bedKd 862.0697
 
   // FIND YOUR OWN: "M303 E-1 C8 S90" to run autotune on the bed at 90 degreesC for 8 cycles.
 #endif // PIDTEMPBED
